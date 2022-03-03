d8379f2899fbbfe203eb2d10f828168b832db2f0
virgo21510023 <virgo_21510023@stimata.ac.id>
2022-03-03 20:12:09 +0700

    Membuat form post nama

---
+++ latihan3.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Metode POST</title>
+</head>
+<body>
+	<form action="latihan4.php" method="POST">
+		Masukkan nama
+		<input type="text" name="nama">
+		<br>
+		<button type="submit" name="submit">
+			Submit
+		</button>
+	</form>
+</body>
+</html>
---
+++ latihan4.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Hasil metode POST</title>
+</head>
+<body>
+	<h1>Selamat datang, <?= $_POST["nama"]; ?></h1>
+</body>
+</html>
